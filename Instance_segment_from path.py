bc65dd0dc4df3ca4a755d2413c8de2e8b76fd259
Bekyilma <byilmalux@gmail.com>
2021-11-14 17:47:58 +0100

    update

---
+++ Instance_segment_from path.py
@@ -0,0 +1,31 @@
+import pixellib
+from pixellib.instance import instance_segmentation
+import cv2
+
+
+
+def main():
+    segmentation_model = instance_segmentation()
+    segmentation_model.load_model('mask_rcnn_coco.h5')
+
+    capture = cv2.VideoCapture("/users/path-to-file")
+    while capture.isOpened():
+        ret, frame = capture.read()
+
+        # Apply instance segmentation
+        res = segmentation_model.segmentFrame(frame, show_bboxes=True)
+        image = res[1]
+
+        cv2.imshow('Instance Segmentation', image)
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+    capture.release()
+    cv2.destroyAllWindows()
+
+
+
+
+if __name__ == '__main__':
+    main()
